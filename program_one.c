643ebd1498e453496f06e42d153f9cd43ed04a97
Drew Guarnera <dguarnera@wooster.edu>
2023-01-16 20:25:34 -0500

    Add inital assignment with testing

---
+++ program_one.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() (
+    INT  sum;
+    /* COMPUTE RESULT
+    sum = 25 + 37 - 19
+    /* DISPLAY RESULTS //
+    printf ("The answer is %i\n" sum);
+    return 0;
+}
---
+++ program_two.c
@@ -0,0 +1,18 @@
+include <studio.h>
+
+def main:
+    printf("Enter x: ")
+    scanf("%1", x);
+
+    printf("Enter y: ");
+    scanf("%1", y);
+
+    int sum = x + y
+
+    printf("x + y is %i\n", sum);
+
+    int difference = x - y
+
+    printf("x - y is %i\n", sum);
+
+    return 0;
